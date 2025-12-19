d13ee1916d741e28465486824ae9039988599335
Elijah <elijah.c.wu@gmail.com>
2025-12-19 05:28:53 +0000

    Update cell_class.py

--- cell_class.py
+++ cell_class.py
@@ -1,17 +1,3 @@
-import pygame
-import random
-pygame.init()
-
-#Colors
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-BLUE = (0, 0, 255)
-
-#Setting up screen
-screen_width = 500
-screen_height = screen_width
-screen = pygame.display.set_mode((screen_width, screen_height))
-
 #Should store data about itself, such as whether it contains a mine, uncovered/covered/flagged, number of adjacent mines
 #
 class cell:
