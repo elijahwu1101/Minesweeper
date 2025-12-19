5ec4f6126d3cf0d51ab3925ccbe7416baaa9af22
Elijah <elijah.c.wu@gmail.com>
2025-12-19 05:29:30 +0000

    Create init.py

---
+++ init.py
@@ -0,0 +1,13 @@
+import pygame
+import random
+pygame.init()
+
+#Colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+BLUE = (0, 0, 255)
+
+#Setting up screen
+screen_width = 500
+screen_height = screen_width
+screen = pygame.display.set_mode((screen_width, screen_height))
