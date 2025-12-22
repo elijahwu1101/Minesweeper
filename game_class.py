b802932f029ad9093bd7e7daf6e2440ab51bc4b9
Elijah <elijah.c.wu@gmail.com>
2025-12-22 20:24:04 +0000

    Update game_class.py

--- game_class.py
+++ game_class.py
@@ -1,1 +1,7 @@
-#Nothing yet...hehe
+class game:
+    def handle_click(self):
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                ...
+    
+        
