7014bdf49295c80173da2c72615f5310f59713f9
Elijah <elijah.c.wu@gmail.com>
2025-12-23 20:45:09 +0000

    Update game_class.py

--- game_class.py
+++ game_class.py
@@ -1,7 +1,23 @@
 class game:
+    def __init__(self, rows, cols, num_mines, cell_size):
+        self.rows = rows
+        self.cols = cols
+        self.num_mines = num_mines
+        self.cell_size = cell_size
+        self.board = board(rows, cols, num_mines)
+        self.cell = cell(...) # Fill in later
+
+    def run(self):
+        ... #Main game loop
+
     def handle_click(self):
         for event in pygame.event.get():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 ...
     
-        
+    def handle_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                self.board.get_cell_at_position()
+                self.cell.handle_mouse_input()
+    #Define mouse_x, mouse_y variables
